8d242930243286e825a6793dd71bf3a43d6f4f20
nicole-gathany <nicole.gathany@gmail.com>
2021-03-31 00:05:59 -0400

    added changes that error and stack-overflow suggested and it still didn't work

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/vaccinesites', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/vaccinesites', { useNewUrlParser: true }, { useUnifiedTopology: true } )
 
 
 app.listen(3000, () => console.log('Server started!'))
