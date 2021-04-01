7e29859f2d27bfd61b8b451732a4664ed7e7a7bd
nicole-gathany <nicole.gathany@gmail.com>
2021-03-31 21:42:50 -0400

    tried to fix. worked for a second. now it's not

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const app = express()
+// console.log("May node be with you") //This worked with the express app and stuff was commented out
+const express = require('express');
+const app = express();
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/vaccinesites', { useNewUrlParser: true }, { useUnifiedTopology: true } )
+mongoose.connect('mongodb://localhost/vaccinesites', { useNewUrlParser: true , useUnifiedTopology: true } )
 
-
-app.listen(3000, () => console.log('Server started!'))
+//what?
+app.listen(1010, function(){
+    console.log('Serving software engineer realness')
+})
