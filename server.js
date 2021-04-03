df68f59aac9e950f82bb7a267aca6ba14cd940bd
nicole-gathany <nicole.gathany@gmail.com>
2021-04-02 23:31:50 -0400

    fixed the issue, may be too soon for mongoose

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // console.log("May node be with you") //This worked with the express app and stuff was commented out
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/vaccinesites/', { useNewUrlParser: true , useUnifiedTopology: true } )
+// mongoose.connect('mongodb://localhost/vaccinesites/', { useNewUrlParser: true , useUnifiedTopology: true } )
 
 app.get('/', (req, res)=> {
     res.sendFile(__dirname + '/index.html') // I forgot the '/' before index.html before
