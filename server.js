378b99c881c9d53c13cec033c6cf59c602f9c520
nicole-gathany <nicole.gathany@gmail.com>
2021-04-04 21:25:18 -0400

    added database connection to mongoose

--- server.js
+++ server.js
@@ -2,6 +2,15 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb+srv://nicole:hButEDlrGdO8nBax@cluster0.txsxy.mongodb.net/vax_waze?retryWrites=true&w=majority', { useUnifiedTopology: true }).then(client=>{
+    
+    console.log("Connected to database")
+    //do something here
+}).catch(error=> console.error(error))
+
+//mongodb+srv://nicole:<password>@cluster0.txsxy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 // const mongoose = require('mongoose')
 
