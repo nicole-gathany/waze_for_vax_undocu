863394e91cb739bc3c8dd933a1f2ab3ce6d56e8d
nicole-gathany <nicole.gathany@gmail.com>
2021-04-04 20:11:02 -0400

    got post requests to work and req.body

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 // console.log("May node be with you") //This worked with the express app and stuff was commented out
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+
 // const mongoose = require('mongoose')
+
+app.use(express.json())
+app.use(bodyParser.json({ type: 'application/*+json' }))
 
 // mongoose.connect('mongodb://localhost/vaccinesites/', { useNewUrlParser: true , useUnifiedTopology: true } )
 
@@ -11,10 +16,11 @@ app.get('/', (req, res)=> {
 
 app.post('/site', (req, res)=>{
     console.log("Helllooooooo, fam!")
-    //post request is not working
+    console.log(req.body)
+    //post request is not working wtih console.log
 })
 
 //what?
 app.listen(2021, ()=>{
-    console.log('Serving software engineer realness')
+    console.log('Serving webpage realness on local host 2021')
 })
