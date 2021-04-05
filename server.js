be5e306feb200d0e0d42f6a5c49055c47fb8fa51
nicole-gathany <nicole.gathany@gmail.com>
2021-04-04 23:01:12 -0400

    connected to database but sites collection not defnied. tbh on feel them on that because the sites collection is defined inside of another environment. i'm not sure how to get the two environments to communicate

--- server.js
+++ server.js
@@ -1,35 +1,45 @@
 // console.log("May node be with you") //This worked with the express app and stuff was commented out
-const bodyParser = require('body-parser');
-const express = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 const app = express();
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 
-MongoClient.connect('mongodb+srv://nicole:hButEDlrGdO8nBax@cluster0.txsxy.mongodb.net/vax_waze?retryWrites=true&w=majority', { useUnifiedTopology: true }).then(client=>{
-    
-    console.log("Connected to database")
+MongoClient.connect(
+  "mongodb+srv://nicole:hButEDlrGdO8nBax@cluster0.txsxy.mongodb.net/vax_waze?retryWrites=true&w=majority",
+  { useUnifiedTopology: true }
+)
+  .then((client) => {
+    console.log("Connected to database");
     //do something here
-}).catch(error=> console.error(error))
+    const db = client.db('vax_waze');
+    const sitesCollection = db.collection('vax_sites')
+  })
+  .catch((error) => console.error(error));
 
 //mongodb+srv://nicole:<password>@cluster0.txsxy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
 // const mongoose = require('mongoose')
 
-app.use(express.json())
-app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(express.json());
+app.use(bodyParser.json({ type: "application/*+json" }));
 
 // mongoose.connect('mongodb://localhost/vaccinesites/', { useNewUrlParser: true , useUnifiedTopology: true } )
 
-app.get('/', (req, res)=> {
-    res.sendFile(__dirname + '/index.html') // I forgot the '/' before index.html before
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html"); // I forgot the '/' before index.html before
+});
 
-app.post('/site', (req, res)=>{
-    console.log("Helllooooooo, fam!")
-    console.log(req.body)
-    //post request is not working wtih console.log
-})
+app.post("/site", (req, res) => {
+
+  sitesCollection.insertOne(req.body)
+  .then(result=>{
+      console.log(result)
+  }
+    ).catch(error=> console.log(error))
+  //post request is not working wtih console.log
+});
 
 //what?
-app.listen(2021, ()=>{
-    console.log('Serving webpage realness on local host 2021')
-})
+app.listen(2021, () => {
+  console.log("Serving webpage realness on local host 2021");
+});
